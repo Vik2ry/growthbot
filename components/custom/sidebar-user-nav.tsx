b72725c31099a9e60160562ee5d5d10cf57f6d07
DankanoFx <mac@MacBook-Pro.local>
2024-11-16 23:14:41 +0800

    removed next auth

--- components/custom/sidebar-user-nav.tsx
+++ components/custom/sidebar-user-nav.tsx
@@ -1,7 +1,6 @@
 'use client';
 import { ChevronUp } from 'lucide-react';
 import Image from 'next/image';
-import { signOut } from 'next-auth/react';
 import { useTheme } from 'next-themes';
 
 import {
